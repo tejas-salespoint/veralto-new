a929b1ac9bac7c721e18504a317715bac40700ae
tejas-salespoint <tejas.lade@salespintinc.com>
2023-11-23 21:58:21 +0530

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,9 @@ export default {
   ],
   theme: {
     backgroundImage: {
-      'bg_img': "url('/src/assets/bg_img.png')",
-      'bg_chatbot': "url('https://daiindustry.blob.core.windows.net/daistrapi/bg_hachchatbot.png')",
-      'bg_hachchatbot': "url('/src/assets/bg_hachchatbot.png')",
+      'bg_img': "url('https://daiindustry.blob.core.windows.net/daistrapi/bg_img.png')",
+      'bg_chatbot': "url('https://daiindustry.blob.core.windows.net/daistrapi/bg_chatbot.png')",
+      'bg_hachchatbot': "url('https://daiindustry.blob.core.windows.net/daistrapi/bg_hachchatbot.png')",
       'bg_topbar': "url('/src/assets/bg_topbar.png')",
       'bg_sidebar': "url('/src/assets/bg_sidebar.png')",
     },
